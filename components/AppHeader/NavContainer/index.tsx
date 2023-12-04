a827b4de598041fd023f863c6a985cd653b4741b
ragnar <mystparkkorea@gmail.com>
2023-12-04 19:26:16 +0900

    nav 구조 스타일링

--- components/AppHeader/NavContainer/index.tsx
+++ components/AppHeader/NavContainer/index.tsx
@@ -1,11 +1,33 @@
 import styles from '@/styles/components/NavContainer.module.css';
+import Link from "next/link";
 
 const NavContainer = () => {
     return (
         <nav className={styles.navContainerWrap}>
-            <div className={styles.navLeft}></div>
-            <div className={styles.navRight}></div>
-            <div></div>
+            <div className={styles.navLeft}>
+                {/*로고 대신 임시*/}
+                <div className={styles.navLogo}></div>
+            </div>
+            <div className={styles.navRight}>
+                {/* TODO 분리 필요*/}
+                <ul className={styles.navUl}>
+                    <li>
+                        <Link href='#'>
+                            소개
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='#'>
+                            유기동물지도
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='#'>
+                            실시간유기동물통계
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
